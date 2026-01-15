6efc2146826792d15b2dd0ca67cf2df5d0211306
Ammar Husain <ammar.husain@alchemytech.ca>
2026-01-15 11:42:42 +0530

    DB-based recipient fetching (deprecated – replaced by API approach)

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -7,3 +7,5 @@ It is completely isolated from the main Streamlit dashboard.
 
 __version__ = "1.0.0"
 
+
+# This module was implemented for fetching the user_email_list from the ERP_DB. (Deprecated)
